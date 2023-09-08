9ac24fdf4959da310c12dca81087c8a296d7b36f
Odeh Simon <simonodeh36@gmail.com>
2023-09-08 02:50:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ function updateTime() {
     const currentDayElement = document.getElementById('currentDayOfTheWeek');
     const currentDate = new Date();
 
-    // Get the current UTC time
-    const utcTimeString = currentDate.toISOString().split('T')[1].split('.')[0];
-    
+    // Get the current time in milliseconds
+    const currentTimeMilliseconds = currentDate.getTime();
+
     // Get the current day of the week
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
 
-    currentTimeElement.textContent = ` ${utcTimeString}`;
+    currentTimeElement.textContent = ` ${currentTimeMilliseconds}`;
     currentDayElement.textContent = ` ${dayOfWeek}`;
 }
 
-// Call updateTime immediately to display the time when the page loads
+// Call updateTime immediately to display the time and day when the page loads
 updateTime();
 
-// Update the time every second (1000 milliseconds)
-setInterval(updateTime, 1000);
+// Update the time and day every millisecond (1 millisecond)
+setInterval(updateTime, 1);
